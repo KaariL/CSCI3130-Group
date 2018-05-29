e120e34fdf3ab1e81481b751c90690a2d20b962c
ajoeman <30836347+ajoeman@users.noreply.github.com>
2018-05-29 11:40:39 -0300

    Created empty Android project

---
+++ app/src/main/java/com/example/group3/csci3130_group3_project/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.group3.csci3130_group3_project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
