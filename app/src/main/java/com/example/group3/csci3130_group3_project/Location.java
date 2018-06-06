88e5c0ac7f5f9e0c58519051df93f7e8683047f6
georgefaraj <georgemilio@gmail.com>
2018-06-06 18:59:15 -0300

    George, Alexa: Modified User Class, Testing Pending

---
+++ app/src/main/java/com/example/group3/csci3130_group3_project/Location.java
@@ -0,0 +1,12 @@
+package com.example.group3.csci3130_group3_project;
+
+public class Location {
+    double xCoord;
+    double yCoord;
+    String type;
+    public Location(double x, double y, String t){
+        xCoord=x;
+        yCoord=y;
+        type=t;
+    }
+}
--- app/src/main/java/com/example/group3/csci3130_group3_project/User.java
+++ app/src/main/java/com/example/group3/csci3130_group3_project/User.java
@@ -1,8 +1,14 @@
 package com.example.group3.csci3130_group3_project;
+import java.util.ArrayList;
 
 public class User {
     String name;
+    String password;
+    Location home;
+    ArrayList<Location> Starred;
+
     public User(String n){
         name = n;
+        Starred = new ArrayList<Location>();
     }
 }
