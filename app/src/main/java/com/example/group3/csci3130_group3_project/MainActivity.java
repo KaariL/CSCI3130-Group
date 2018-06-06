ec86b5e156e207cb952258d33dbfc49c13c61e28
georgefaraj <georgemilio@gmail.com>
2018-06-06 15:24:34 -0300

    George, Alexa: Upload a single user to Firebase

--- app/src/main/java/com/example/group3/csci3130_group3_project/MainActivity.java
+++ app/src/main/java/com/example/group3/csci3130_group3_project/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.group3.csci3130_group3_project;
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.view.View;
+import com.google.firebase.database.*;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+    }
+
+    public void onSubmit(View view) {
+        User u = new User("Pablo");
+        mDatabase.child("UserList").child("001").setValue(u);
     }
 }
---
+++ app/src/main/java/com/example/group3/csci3130_group3_project/User.java
@@ -0,0 +1,8 @@
+package com.example.group3.csci3130_group3_project;
+
+public class User {
+    String name;
+    public User(String n){
+        name = n;
+    }
+}
