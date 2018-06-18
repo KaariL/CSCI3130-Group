ff6c18aa6bda2c1f9e73361f5db091944022f773
ajoeman <30836347+ajoeman@users.noreply.github.com>
2018-06-18 13:27:44 -0300

    Register button now adds user to firebase and gives appropriate error messages. AM KB

--- app/src/main/java/com/example/group3/csci3130_group3_project/RegisterActivity.java
+++ app/src/main/java/com/example/group3/csci3130_group3_project/RegisterActivity.java
@@ -1,27 +1,67 @@
 package com.example.group3.csci3130_group3_project;
 
 import android.graphics.Color;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseUser;
+
 
 public class RegisterActivity extends AppCompatActivity {
     protected registrationValidator validator;
     protected boolean formIsValid;
-
+    private FirebaseAuth mAuth;
+    private static final String TAG = "Register";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        mAuth = FirebaseAuth.getInstance();
         validator = new registrationValidator();
         formIsValid = true;
     }
 
+    protected void createNewUser(String email, String password){
+        if(!formIsValid){
+            return;
+        }
+        mAuth.createUserWithEmailAndPassword(email, password).
+                addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            
+                        }
+                        else {
+                            Log.w(TAG, "createUserWithEmail:failed");
+                            if (task.getException() instanceof FirebaseAuthUserCollisionException) {
+                                Toast.makeText(RegisterActivity.this, "This email is already in use. Please try again", Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(RegisterActivity.this, "Authenitifcation failed",
+                                        Toast.LENGTH_SHORT).show();
+
+                            }
+                        }
+                    }
+                });
+    }
 
     public void validateForm(View view){
         this.restoreFieldDefaults();
+        formIsValid = true;
         if(!validator.usernameIsValid(this.getUsernameField())){
             TextView username = findViewById(R.id.usernameText);
             username.setText(R.string.userRegErr);
@@ -45,6 +85,9 @@ public class RegisterActivity extends AppCompatActivity {
             confirmPass.setText(R.string.confirmPassRegErr);
             confirmPass.setTextColor(Color.RED);
             this.formIsValid = false;
+        }
+        if(formIsValid){
+            createNewUser(this.getEmailField(), getPasswordField());
         }
 
     }
