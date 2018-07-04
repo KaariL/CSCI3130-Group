585109a769f8a10905bf3a359bef63944c873705
KaariL <kaari.landry@gmail.com>
2018-07-04 15:20:40 -0300

    Complete 1 instrumented test for map
    GF + KL

--- app/src/androidTest/java/com/example/group3/csci3130_group3_project/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/group3/csci3130_group3_project/ExampleInstrumentedTest.java
@@ -1,5 +1,7 @@
 package com.example.group3.csci3130_group3_project;
 
+import android.app.Activity;
+import android.app.Instrumentation;
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.espresso.Espresso;
@@ -12,6 +14,7 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
@@ -27,10 +30,14 @@ public class ExampleInstrumentedTest {
     String goodUsername = "kaari.landry@gmail.com";
     String goodPassword = "Agr34Tpw!";
     @Rule
-    public ActivityTestRule<CredentialActivity> activityActivityTestRule =
+    public ActivityTestRule<CredentialActivity> mActivityTestRule =
             new ActivityTestRule<CredentialActivity>(CredentialActivity.class);
+
+    private Activity loginActivity = null;
     @Before
-    public void logIn(){
+    public void logIn() throws Exception  {
+        loginActivity = mActivityTestRule.getActivity();
+        //Activity startActivity =  CredentialActivity.getActivity();
         Espresso.onView(withId(R.id.editText_name)).perform(ViewActions.typeText(goodUsername));
         Espresso.onView(withId(R.id.editText_psw)).perform(ViewActions.typeText(goodPassword));
         Espresso.onView(withId(R.id.button_login)).perform(ViewActions.click());
@@ -39,8 +46,9 @@ public class ExampleInstrumentedTest {
     }
 
     @Test
-    public void isOnMapActivity(){
-        Espresso.onData(withId(R.id.map)).check(matches(isDisplayed()));
+    public void isOnMapActivity()throws InterruptedException{
+        Thread.sleep(5000);
+        Espresso.onView(withId(R.id.map)).check(matches(isDisplayed()));
     }
 
 }
