a720c8616e65b82e87ba338dc35d8721dba80469
georgefaraj <georgemilio@gmail.com>
2018-07-04 09:41:48 -0300

    KL GF, Added ability check for change in GPS settings and request updates accordingly, added permission requests. PENDING: Severe refactoring is needed

--- app/src/main/java/com/example/group3/csci3130_group3_project/MainActivity.java
+++ app/src/main/java/com/example/group3/csci3130_group3_project/MainActivity.java
@@ -8,6 +8,7 @@ import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
@@ -89,6 +90,15 @@ public class MainActivity extends BaseActivity implements OnMapReadyCallback {
             alertDialog.setCanceledOnTouchOutside(false);
             alertDialog.show();
         }
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    1);
+            return;
+        }
+        mLocationManager.requestSingleUpdate(LocationManager.GPS_PROVIDER, (android.location.LocationListener) new MyLocationListenerGPS(), null);
+
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -131,19 +141,91 @@ public class MainActivity extends BaseActivity implements OnMapReadyCallback {
         }
     }
 
+
     public void FIND(View view) {
         EditText locationSearch = (EditText) findViewById(R.id.searchBar);
+
+
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Error("Permission Error");
             // TODO: Consider calling
-            locationSearch.setText("Error");
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    1);
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        if(mLocationManager==null){
+            Error("LM Error");
+            return;
+        }
+        if(!mLocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)){
+            Error("GPS Error");
             return;
         }
 
         Location L = mLocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        if(L==null) {
+            Error("L Error");
+            return;
+        }
+
         LatLng HERE = new LatLng(L.getLatitude(), L.getLongitude());
         mMap.addMarker(new MarkerOptions().position(HERE).title("I AM HERE"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(HERE));
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
 
+            // other 'case' lines to check for other
+            // permissions this app might request.
+        }
+    }
+
+    public void Error(String e){
+        EditText locationSearch = (EditText) findViewById(R.id.searchBar);
+        locationSearch.setText(e);
+    }
+
+    public class MyLocationListenerGPS implements LocationListener {
+        @Override
+        public void onLocationChanged(Location location) {
+            Error("Location Changed!");
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+            Error("Status change");
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+            Error("provider change");
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+            Error("provider died");
+        }
+    }
 }
